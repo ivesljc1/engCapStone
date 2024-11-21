a44ad57da5120a72bacf79d75e25a43283cf7d0e
ivesljc.eth <ivesljc@gmail.com>
2024-11-21 14:17:56 -0500

    feat: login & signup front end

--- wellpathai/app/page.jsx
+++ wellpathai/app/page.jsx
@@ -1,7 +1,14 @@
-"use client";
-
-import LoginPage from "./login/page";
+import Link from 'next/link'
 
 export default function Home() {
-  return <LoginPage />;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Link 
+        href="/login" 
+        className="px-6 py-3 text-lg font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 transition-colors duration-300"
+      >
+        Sign In
+      </Link>
+    </div>
+  )
 }
