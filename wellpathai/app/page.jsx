a3921083eee85f193f14d0235777cfac8014546a
LEYANG XING <lxing24@uwo.ca>
2024-11-28 16:55:32 -0500

    feat: Entrance set as login page

--- wellpathai/app/page.jsx
+++ wellpathai/app/page.jsx
@@ -1,14 +1,6 @@
-import Link from 'next/link'
-
+import LoginPage from "./login/page"
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Link 
-        href="/login" 
-        className="px-6 py-3 text-lg font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 transition-colors duration-300"
-      >
-        Sign In
-      </Link>
-    </div>
+    <LoginPage />
   )
 }
